614e62c610d1a4ca218d789b327f848ce3c79807
Sergio <spn1994@hotmail.com>
2024-02-27 16:23:17 -0300

    prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,18 @@
 import fastify from "fastify";
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  //quais dados passar na hora de criar, atraves data
+  data: {
+    name: 'Sergio Nahirny',
+    email: 'sergio@email.com.br',
+  },
+})
+
+// ORM - Object Relational Mapper
+
+// ORM - Object Relational Mapping (mapear tudo q tem dentro em objetos no DBA)
