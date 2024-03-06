ca0c01509e226beba4c10a727cbf5908c9bc2f2f
Sergio <spn1994@hotmail.com>
2024-03-06 15:19:33 -0300

    hash senha

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,8 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
+import { hash } from 'bcryptjs'
 import { prisma } from '@/lib/prisma'
 import { z } from 'zod'
+
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -10,14 +12,29 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   })
 
   const { name, email, password } = registerBodySchema.parse(request.body)
+  //hash vai gerar dado impossivel reverter, salt eu escolhi 6, a cada round ele gera um hash e fica mais dificil
+  const password_hash = await hash(password, 6)
+//find unique só busca registro unicos ou chave primaria
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  })
+
+  if (userWithSameEmail) {
+    //409 status conflito
+    return reply.status(409).send()
+  }
 
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password,
+      password_hash,
     },
   })
 
   return reply.status(201).send()
 }
+//controler é quem lida com entrada de dados num requisição, tudo q faz parte de resposta e requisição
+// coloca na pasta http
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from 'fastify'
 import { register } from './controllers/register'
-
+//plugin tem que ser assincrono
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
 }
