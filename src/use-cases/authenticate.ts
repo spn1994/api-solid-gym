55bc2c7664a4d10de75e7968eb2d08e7e3212886
Sergio <spn1994@hotmail.com>
2024-03-22 14:33:25 -0300

    authenticate

---
+++ src/use-cases/authenticate.ts
@@ -0,0 +1,40 @@
+import { UsersRepository } from '@/repositories/users-repository'
+import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
+import { User } from '@prisma/client'
+import { compare } from 'bcryptjs'
+
+//se ela se auntenticar eu vou devolver
+interface AuthenticateUseCaseRequest {
+  email: string
+  password: string
+}
+
+interface AuthenticateUseCaseResponse {
+  user: User
+}
+
+
+export class AuthenticateUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
+    const user = await this.usersRepository.findByEmail(email)
+//preciso buscar usiarui no banco e comprara se a senha salva é real
+    if (!user) {
+      throw new InvalidCredentialsError()
+    }
+
+    const doestPasswordMatches = await compare(password, user.password_hash)
+
+    if (!doestPasswordMatches) {
+      throw new InvalidCredentialsError()
+    }
+
+    return {
+      user,
+    }
+  }
+}
